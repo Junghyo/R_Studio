097be3028b86c07f68e73d7415230c60b0ff498f
Junghyo <tjwjdgy123@gamil.com>
2017-07-13 20:39:07 +0900

    chapter07 ing

--- Rscript/chapter/chapter07/chap07.R
+++ Rscript/chapter/chapter07/chap07.R
@@ -107,4 +107,74 @@ summary(dataset$age)
 boxplot(dataset$age) # boxplot 평균연령 : 40대 중반
 
 # 가독성을 위한 코딩변경
+# 현재 resident변수 데이터값들이 1~5 숫자로 되어있어 거주지를 한번에 파악하기 불가능하다.
+# 따라서 코딩변경 작업이 필요하다.
+table(dataset$resident) # 1~5 외 값은 없는 것으로 보아 극단치는 존재하지 않는다.
+summary(dataset$resident) # 결측치 15개 존재
+dataset$resident2[dataset$resident==1] <- '서울'
+dataset$resident2[dataset$resident==2] <- '인천'
+dataset$resident2[dataset$resident==3] <- '대전'
+dataset$resident2[dataset$resident==4] <- '대구'
+dataset$resident2[dataset$resident==5] <- '기타'
+dataset[c("resident", "resident2")]
 
+# job 컬럼 코딩변경
+summary(dataset$job) # 결측치 12개 존재
+dataset$job2[dataset$job==1] <- "공무원"
+dataset$job2[dataset$job==2] <- "회사원"
+dataset$job2[dataset$job==3] <- "개인사업"
+dataset[c("job","job2")]
+
+# 척도변경을 위한 코딩
+# 나이변수 : 21~69 분포
+# <30 : 장년층, 31~55 : 중년층, 56< : 장년층으로 age2라는 범주형변수를 만들고 싶다.
+summary(dataset$age) # 결측치 없음
+dataset$age2[dataset$age<30] <- "청년층"
+dataset$age2[dataset$age>=30 & dataset$age<56] <- "중년층"
+dataset$age2[dataset$age>=56] <- "장년층"
+dataset[c("age","age2")]
+head(dataset)
+
+# 역코딩을 위한 변경
+# dataset$survay(만족도)의 설문지 문항을
+# 1 매우 만족, 2 만족, 3 보통, 4 불만, 5 매우 불만
+# 형태로 작성되었을 때 변경하지 않고 코딩하게 되면 점수화가 역순으로 되어버린다.
+# 따라서 매우 만족~매우 불만의 순서를 역순으로 변경해야 하는데 이러한 작업을 
+# 역코딩이라고 한다.
+# 이러한 역코딩을 하기 위해서는 "(범주수+1)-데이터값"으로 처리하면 된다.
+
+# 만족도(survey)를 긍정순서로 역코딩
+summary(dataset$survey)  # 결측치, 극단치 없음
+head(dataset$survey) #  기존  : [1] 1 2 4 2 1 2
+dataset$survey <- 6-dataset$survey
+head(dataset$survey) # 역코딩 : [1] 5 4 2 4 5 4
+
+# 탐색적 분석을 위한 시각화
+
+# 범주형 vs 연속형
+# 범주형 : 명목척도, 서열척도
+# 연속형 : 등간척도, 비율척도
+
+# 데이터 셋 가져오기. Part-II,  new_data.csv
+new_data <- read.csv(file.choose(), header=T)
+new_data
+str(new_data) # 11개 변수, 217개 관측치
+mode(new_data); class(new_data)  # list, data.frame
+levels(new_data$resident2) <- c("1.서울특별시", "2.인천광역시", "3.대전광역시",
+                                "4.대구광역시", "5.시군구")
+new_data$resident2[new_data$resident==1] <- "1.서울특별시"
+new_data$resident2[new_data$resident==2] <- "2.인천광역시"
+new_data$resident2[new_data$resident==3] <- "3.대전광역시"
+new_data$resident2[new_data$resident==4] <- "4.대구광역시"
+new_data$resident2[new_data$resident==5] <- "5.시군구"
+new_data$job2[new_data$job==1] <- "공무원"
+new_data$job2[new_data$job==2] <- "회사원"
+new_data$job2[new_data$job==3] <- "개인사업"
+summary(new_data$position)
+new_data$postion2[new_data$position==1] <- "1급"
+new_data$postion2[new_data$position==2] <- "2급"
+new_data$postion2[new_data$position==3] <- "3급"
+new_data$postion2[new_data$position==4] <- "4급"
+new_data$postion2[new_data$position==5] <- "5급"
+new_data$postion2 <- as.factor(new_data$postion2)
+write.csv(new_data, "new_data.csv")
